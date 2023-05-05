1e4a223f298665314dc6fe00c3cac9eecfd4ecd5
DimaKrupenko <dimakrupenko77@gmail.com>
2023-05-05 15:26:02 +0300

    upd

---
+++ app.ts
@@ -0,0 +1,91 @@
+const button = document.querySelector('button')
+const input1 = document.getElementById('num1')! as HTMLInputElement
+const input2 = document.getElementById('num2')! as HTMLInputElement
+
+function add(num1: number, num2: number) {
+    return num1 + num2
+}
+
+button.addEventListener('click', function () {
+    console.log(add(+input1.value, +input2.value))
+})
+
+
+let age: number = 50;
+let name1: string = 'Max';
+let toggle: boolean = true;
+let empty: null = null;
+let notInitialize: undefined = undefined;
+let callback = (a: number) => { return 100 + a };
+
+let anything: any = -20;
+anything = 'Text';
+anything = {};
+
+let some: any;
+some = 'Text';
+
+let str: string;
+
+str = some;
+
+let person: readonly [string, number] = ['Max', 21];
+
+enum LoadingStatus {
+  LOADING = 'LOADING',
+  READY = 'READY',
+}
+
+let status1: LoadingStatus = LoadingStatus.LOADING;
+
+if (status1 === LoadingStatus.LOADING) {
+  console.log('Загружается');
+} else if (status1 === LoadingStatus.READY) {
+  console.log('Загружено');
+}
+
+let any: string | number = 21
+
+let status2: 'enable' | 'disable' = 'enable';
+
+
+function showMessage(message: string) {
+  console.log(message);
+}
+
+function calc(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+function customError(): never {
+  throw new Error('Error');
+}
+
+interface Page {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: 'open' | 'close';
+  details?: {
+    createAt: string;
+    updateAt: string;
+  };
+}
+
+const page1: Page = {
+  title: 'The awesome page',
+  likes: 100,
+  accounts: ['Max', 'Anton', 'Nikita'],
+  status: 'open',
+  details: {
+    createAt: '2021-01-01',
+    updateAt: '2021-05-01',
+  }
+}
+
+const page2: Page = {
+  title: 'Python or Js',
+  likes: 5,
+  accounts: ['Alex'],
+  status: 'close',
+}
